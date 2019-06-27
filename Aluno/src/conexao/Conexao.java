4902fc813d4c0eb011b583faafbcfbef1f4762c3
Administrador <Administrador@10.10.4.2>
2019-06-27 16:43:31 -0300

    2.0

--- Aluno/src/conexao/Conexao.java
+++ Aluno/src/conexao/Conexao.java
@@ -1,3 +1,8 @@
+package conexao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Aluno;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
